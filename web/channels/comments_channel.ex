eaae54520a05785601da5287355399988f5397c0
nicfisher <n.f@live.com.au>
2020-03-14 19:28:47 +1100

    set up most of the websocket for comments

---
+++ web/channels/comments_channel.ex
@@ -0,0 +1,36 @@
+defmodule Discuss.CommentsChannel do
+  use Discuss.Web, :channel
+  alias Discuss.{Topic, Comment}
+
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+    # This will get the topic with the "topic_id" and then load the comment associations
+    topic = Topic
+      |> Repo.get(topic_id)
+      |> Repo.preload(:comments)
+
+    {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
+  end
+
+  def handle_in(name, %{"content" => content}, socket) do
+    IO.puts("++++")
+    IO.inspect(content)
+
+    topic = socket.assigns.topic
+
+    # TODO - how does build_assoc work
+    changeset = topic
+      |> build_assoc(:comments)
+      |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        broadcast!(socket, "comments:#{socket.assigns.topic.id}:new",
+          %{comment: comment}
+        )
+        {:reply, :ok, socket}
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
+  end
+end
